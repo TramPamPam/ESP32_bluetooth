76abf333ed373b8c0e57b34d7bf816554cd8f0ff
Oleksandr Bezpalchuk <oleksandr.bezpalchuk@ekreative.com>
2018-09-09 11:50:24 +0300

    clean up

--- Zori/AppDelegate.swift
+++ Zori/AppDelegate.swift
@@ -19,7 +19,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         _ = CoreDataStackImplementation.shared
         NFX.sharedInstance().start()
-        Converter().example()
         return true
     }
     
